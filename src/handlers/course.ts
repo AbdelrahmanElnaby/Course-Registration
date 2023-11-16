959e1d37490bdca5b3b39d25efd355b50d495c94
Abdelrahman <abdelrahmaelnaby@gmail.com>
2023-11-16 22:13:16 +0100

    add course handler

---
+++ src/handlers/course.ts
@@ -0,0 +1,104 @@
+import course from "../models/Course"
+import { Request, Response } from "express"
+import {responseHandle, errorHandle} from "../utilities/globalmeth"
+import {T_Course,T_CourseClass,T_CourseClassStudent,T_CourseClassTeacher} from "../utilities/types"
+
+export const index = async (_req:Request,res:Response):Promise<void> =>{
+    try{
+        const result = await course.index();
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "index");
+        responseHandle(error,res);
+    }
+}
+
+export const show = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const pId  = Number(req.params.id);
+        const result = await course.show(pId) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "show");
+        responseHandle(error,res);
+    }
+}
+
+export const create = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const body:T_Course = req.body;
+        const result = await course.create(body);
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "create");
+        responseHandle(error,res);
+    }
+}
+
+export const update = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const pId = Number(req.params.id);
+        const body:T_Course = req.body;
+        const result = await course.edit(pId,body);
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "update");
+        responseHandle(error,res);
+    }
+}
+
+export const deleteItem = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const pId  = Number(req.params.id);
+        const result = await course.delete(pId) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "deleteItem");
+        responseHandle(error,res);
+    }
+}
+export const addCourseClass = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const pId  = Number(req.params.id);
+        const body:T_CourseClass = req.body;
+        const result = await course.addCourseClass(pId,body) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "show");
+        responseHandle(error,res);
+    }
+}
+
+export const addCourseClassTeacher = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const pId  = Number(req.params.id);
+        const className = req.params.name;
+        const body:T_CourseClass = req.body;
+        const result = await course.addCourseClassTeacher(pId,className,body) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "show");
+        responseHandle(error,res);
+    }
+}
+
+export const addCourseClassStudent = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const pId  = Number(req.params.id);
+        const className = req.params.name;
+        const body:T_CourseClass = req.body;
+        const result = await course.addCourseClassStudent(pId,className,body) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "show");
+        responseHandle(error,res);
+    }
+}
--- src/utilities/globalmeth.ts
+++ src/utilities/globalmeth.ts
@@ -1,8 +1,21 @@
-
+import { Response } from "express";
 
 export const errorHandle = (err:unknown,owner:string,method?:string):Error=>{
-    const error = err instanceof Error ? (err as Error) : new Error('no error reference'); 
+    const error = err instanceof Error ? err : new Error('no error reference'); 
     const prefix = '-> '+ owner + method ?? '' + ':: '
     error.message = prefix + error.message; 
     return error;
 }
+
+export const responseHandle = (result:Object|null,res:Response,message?:string):void=>{
+    if(! result ){
+        res.status(300).send(message ?? "Bad User Request");
+    }
+    else if(result instanceof Error){
+        res.status(400).send(message ?? "Internal Server Errror");
+    }
+    else{
+        res.status(200).json(result);
+    }
+}
+
