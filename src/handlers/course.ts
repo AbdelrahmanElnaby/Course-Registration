d2870daca45bbdcb671c0d19ed1cc9ed03f5e90a
Abdelrahman <abdelrahmaelnaby@gmail.com>
2023-11-18 13:49:43 +0100

    update handler folder :1

--- src/handlers/course.ts
+++ src/handlers/course.ts
@@ -1,7 +1,20 @@
-import course from "../models/Course"
+import course from "../models/course"
 import { Request, Response } from "express"
 import {responseHandle, errorHandle} from "../utilities/globalmeth"
 import {T_Course,T_CourseClass,T_CourseClassStudent,T_CourseClassTeacher} from "../utilities/types"
+
+//#region Course Handlers
+export const create = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const body:T_Course = req.body;
+        const result = await course.create(body);
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "create");
+        responseHandle(error,res);
+    }
+}
 
 export const index = async (_req:Request,res:Response):Promise<void> =>{
     try{
@@ -22,18 +35,6 @@ export const show = async (req:Request,res:Response):Promise<void> =>{
     }
     catch(err){
         const error = errorHandle(err, __filename, "show");
-        responseHandle(error,res);
-    }
-}
-
-export const create = async (req:Request,res:Response):Promise<void> =>{
-    try{
-        const body:T_Course = req.body;
-        const result = await course.create(body);
-        responseHandle(result,res);
-    }
-    catch(err){
-        const error = errorHandle(err, __filename, "create");
         responseHandle(error,res);
     }
 }
@@ -62,12 +63,14 @@ export const deleteItem = async (req:Request,res:Response):Promise<void> =>{
         responseHandle(error,res);
     }
 }
-
+//#region
+ 
+//#region CourseClasses Hnadlers
 export const addCourseClass = async (req:Request,res:Response):Promise<void> =>{
     try{
-        const pId  = Number(req.params.id);
+        const courseId  = Number(req.params.courseId);
         const body:T_CourseClass = req.body;
-        const result = await course.addCourseClass(pId,body) ;
+        const result = await course.addCourseClass(courseId,body) ;
         responseHandle(result,res);
     }
     catch(err){
@@ -76,12 +79,66 @@ export const addCourseClass = async (req:Request,res:Response):Promise<void> =>{
     }
 }
 
+export const getCourseClasses = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const result = await course.getCourseClasses(courseId) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "getCourseClasses");
+        responseHandle(error,res);
+    }
+}
+
+export const getSpecificCourseClass = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const className  = req.params.className;
+        const result = await course.getSpecificCourseClass(courseId,className) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "getSpecificCourseClass");
+        responseHandle(error,res);
+    }
+}
+
+export const editCourseClass = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const className  = req.params.className;
+        const body:T_CourseClass = req.body;
+        const result = await course.editCourseClass(courseId,className,body) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "editCourseClass");
+        responseHandle(error,res);
+    }
+}
+
+export const deleteCourseClass = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const className  = req.params.className;
+        const result = await course.deleteCourseClass(courseId,className) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "deleteCourseClass");
+        responseHandle(error,res);
+    }
+}
+//#region 
+
+//#region CourseClassTeachers handlers
 export const addCourseClassTeacher = async (req:Request,res:Response):Promise<void> =>{
     try{
-        const pId  = Number(req.params.id);
-        const className = req.params.name;
-        const body:T_CourseClass = req.body;
-        const result = await course.addCourseClassTeacher(pId,className,body) ;
+        const courseId  = Number(req.params.courseId);
+        const className = req.params.className;
+        const body:T_CourseClassTeacher = req.body;
+        const result = await course.addCourseClassTeacher(courseId,className,body) ;
         responseHandle(result,res);
     }
     catch(err){
@@ -90,12 +147,70 @@ export const addCourseClassTeacher = async (req:Request,res:Response):Promise<vo
     }
 }
 
+export const getCourseClassTeachers = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const className = req.params.className;
+        const result = await course.getCourseClassTeachers(courseId,className) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "getCourseClassTeachers");
+        responseHandle(error,res);
+    }
+}
+
+export const getSpecificCourseClassTeacher = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const className = req.params.className;
+        const teacherId  = Number(req.params.teacherId);
+        const result = await course.getSpecificCourseClassTeacher(courseId,className,teacherId) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "getSpecificCourseClassTeacher");
+        responseHandle(error,res);
+    }
+}
+
+export const editCourseClassTeacher = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const className = req.params.className;
+        const teacherId  = Number(req.params.teacherId);
+        const body:T_CourseClassTeacher = req.body;
+        const result = await course.editCourseClassTeacher(courseId,className,teacherId,body) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "editCourseClassTeacher");
+        responseHandle(error,res);
+    }
+}
+
+export const deleteCourseClassTeacher = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const className = req.params.className;
+        const teacherId  = Number(req.params.teacherId);
+        const result = await course.deleteCourseClassTeacher(courseId,className,teacherId) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "deleteCourseClassTeacher");
+        responseHandle(error,res);
+    }
+}
+//#region 
+
+//#region CourseClassStudents handlers
 export const addCourseClassStudent = async (req:Request,res:Response):Promise<void> =>{
     try{
-        const pId  = Number(req.params.id);
-        const className = req.params.name;
-        const body:T_CourseClass = req.body;
-        const result = await course.addCourseClassStudent(pId,className,body) ;
+        const courseId  = Number(req.params.courseId);
+        const className = req.params.className;
+        const body:T_CourseClassStudent = req.body;
+        const result = await course.addCourseClassStudent(courseId,className,body) ;
         responseHandle(result,res);
     }
     catch(err){
@@ -103,3 +218,60 @@ export const addCourseClassStudent = async (req:Request,res:Response):Promise<vo
         responseHandle(error,res);
     }
 }
+
+export const getCourseClassStudents= async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const className = req.params.className;
+        const result = await course.getCourseClassStudents(courseId,className) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "getCourseClassStudents");
+        responseHandle(error,res);
+    }
+}
+
+export const getSpecificCourseClassStudents= async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const className = req.params.className;
+        const studentId  = Number(req.params.studentId);
+        const result = await course.getSpecificCourseClassStudent(courseId,className,studentId) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "getSpecificCourseClassStudents");
+        responseHandle(error,res);
+    }
+}
+
+export const editCourseClassStudent = async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const className = req.params.className;
+        const studentId  = Number(req.params.studentId);
+        const body:T_CourseClassStudent = req.body;
+        const result = await course.editCourseClassStudent(courseId,className,studentId,body) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "editCourseClassStudent");
+        responseHandle(error,res);
+    }
+}
+
+export const deleteCourseClassStudents= async (req:Request,res:Response):Promise<void> =>{
+    try{
+        const courseId  = Number(req.params.courseId);
+        const className = req.params.className;
+        const studentId  = Number(req.params.studentId);
+        const result = await course.deleteCourseClassStudent(courseId,className,studentId) ;
+        responseHandle(result,res);
+    }
+    catch(err){
+        const error = errorHandle(err, __filename, "deleteCourseClassStudents");
+        responseHandle(error,res);
+    }
+}
+//#region 
